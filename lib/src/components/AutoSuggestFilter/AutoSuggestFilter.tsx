7e2087551658a7ce4bd050b5bc085fb994f9a788
Rodrigo Limões <rolimoes@gmail.com>
2022-09-02 23:35:49 -0300

    Changing the text field style when find the suggestion.

--- lib/src/components/AutoSuggestFilter/AutoSuggestFilter.tsx
+++ lib/src/components/AutoSuggestFilter/AutoSuggestFilter.tsx
@@ -2,26 +2,50 @@ import React from "react";
 import TextField from "../TextField/TextField";
 import { Container } from "./style";
 
+interface Data {
+  [key: string]: any;
+}
+
 interface AutoSuggestFilterStateProps {
   inputProps: {
     placeholder: string;
     value: string;
     onChange: (value: string) => void;
   };
+  suggestions: Array<Data> | Array<string>;
 }
 
-interface AutoSuggestFilterDispatchProps {}
+interface AutoSuggestFilterDispatchProps {
+  onSuggestionsFetch: (value: string) => void;
+}
 
 type AutoSuggestFilterProps = AutoSuggestFilterDispatchProps &
   AutoSuggestFilterStateProps;
 
 const AutoSuggestFilter: React.FC<AutoSuggestFilterProps> = ({
   inputProps,
+  suggestions,
+  onSuggestionsFetch,
 }) => {
   const { placeholder, value, onChange } = inputProps;
+
+  const onChangeTextField = (value: string) => {
+    onChange(value);
+    onSuggestionsFetch(value);
+  };
+
+  const isShowDropdown = Array.isArray(suggestions)
+    ? suggestions.length > 0
+    : false;
+
   return (
     <Container>
-      <TextField placeholder={placeholder} value={value} onChange={onChange} />
+      <TextField
+        placeholder={placeholder}
+        value={value}
+        onChange={onChangeTextField}
+        isShowDropdown={isShowDropdown}
+      />
     </Container>
   );
 };
